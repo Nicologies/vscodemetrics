74ad09a38fad7b0f7333df39dc38df81d39ad760
ShanshanDu <nicoleflopy@gmail.com>
2015-05-23 21:39:46 +0800

    Auto detect the executable path of code metrics.exe

---
+++ vscodemetrics-agent/src/main/java/com/nicologies/vscodemetrics/CodeMetricsPropertiesExtension.java
@@ -0,0 +1,37 @@
+package com.nicologies.vscodemetrics;
+
+import com.nicologies.vscodemetrics.common.CodeMetricConstants;
+import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
+import jetbrains.buildServer.agent.AgentLifeCycleListener;
+import jetbrains.buildServer.agent.BuildAgent;
+import jetbrains.buildServer.util.EventDispatcher;
+import jetbrains.buildServer.util.positioning.PositionAware;
+import jetbrains.buildServer.util.positioning.PositionConstraint;
+import org.jetbrains.annotations.NotNull;
+
+public class CodeMetricsPropertiesExtension extends AgentLifeCycleAdapter implements PositionAware {
+
+  @NotNull
+  private final MetricsExeSearcher mySearcher;
+
+  public CodeMetricsPropertiesExtension(@NotNull final EventDispatcher<AgentLifeCycleListener> events,
+                                        @NotNull final MetricsExeSearcher searcher) {
+    mySearcher = searcher;
+    events.addListener(this);
+  }
+
+  @NotNull
+  public String getOrderId() {
+    return CodeMetricConstants.RunnerType;
+  }
+
+  @NotNull
+  public PositionConstraint getConstraint() {
+    return PositionConstraint.last();
+  }
+
+  @Override
+  public void agentInitialized(@NotNull final BuildAgent agent) {
+    mySearcher.search(agent.getConfiguration());
+  }
+}
---
+++ vscodemetrics-common/src/main/java/com/nicologies/vscodemetrics/common/CodeMetricsVersion.java
@@ -0,0 +1,41 @@
+package com.nicologies.vscodemetrics.common;
+
+import jetbrains.buildServer.requirements.Requirement;
+import jetbrains.buildServer.requirements.RequirementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public enum CodeMetricsVersion {
+
+  not_specified("not_specified", "Any Detected") {
+    @Override
+    public Requirement createRequirement() {
+      return new Requirement(SettingsKeys.CmdFileVersionProperty, null, RequirementType.EXISTS);
+    }
+  },
+  v12_0("12.0", "12.0"),
+  v14_0("14.0", "14.0");
+
+  private final String myTechnicalVersionPrefix;
+  private final String myDisplayName;
+
+  CodeMetricsVersion(final String technicalVersionPrefix, final String displayName) {
+    myTechnicalVersionPrefix = technicalVersionPrefix;
+    myDisplayName = displayName;
+  }
+
+  @NotNull
+  public String getTechnicalVersionPrefix() {
+    return myTechnicalVersionPrefix;
+  }
+
+  @NotNull
+  public String getDisplayName() {
+    return myDisplayName;
+  }
+
+  @Nullable
+  public Requirement createRequirement(){
+    return new Requirement(SettingsKeys.CmdFileVersionProperty, getTechnicalVersionPrefix(), RequirementType.STARTS_WITH);
+  }
+}
