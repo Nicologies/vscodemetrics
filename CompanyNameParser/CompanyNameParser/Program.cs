e7098ee94b32c6a73779498c1684364f42cd0bd2
ShanshanDu <nicoleflopy@gmail.com>
2016-01-31 20:40:59 +0800

    Add project to parse comany name from assembly

---
+++ CompanyNameParser/CompanyNameParser/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyNameParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Environment.Exit(-1);
+            }
+            try
+            {
+                var verInfo = FileVersionInfo.GetVersionInfo(args[0]);
+                Console.Write(verInfo.CompanyName);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error: " + ex);
+            }
+        }
+    }
+}
